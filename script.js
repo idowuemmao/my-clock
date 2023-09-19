7cef0c73ffe39c06d61334edfc4ac84ebd65f71b
Idowu Emmanuel <idowuemmao@gmail.com>
2023-09-19 14:47:10 +0100

    clone done

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000); //To call a function every second
+
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+function setClock() {
+  const currentDate = new Date(); //gives you the current date
+  const secondsRatio = currentDate.getSeconds() / 60; //because there is 60seconds
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
